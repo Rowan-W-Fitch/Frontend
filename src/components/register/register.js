32ef13c437c51680ad02bc5568a663289d9fdd5f
Rowan-W-Fitch <rfitch@oxy.edu>
2020-11-15 15:02:59 -0800

    heroku

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -119,7 +119,7 @@ class Register extends React.Component{
             <Col>
             </Col>
             <Col>
-              <Card style={{ marginTop: "1%", marginLeft: "40%",  width: "350px"}}>
+              <Card style={{ marginTop: "1%", marginLeft: "40%",  width: "400px"}}>
                 <Card.Header>
                   <img src = {logo2} width="50" height="50"/><b>Register Your Optimal Stoke Account</b>
                 </Card.Header>
